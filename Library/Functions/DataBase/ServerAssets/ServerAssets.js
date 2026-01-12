935b6b09dcb5a7489abffeb34b80e700dcb7a55a
Elite Robust Ontology <erandix9@gmail.com>
2026-01-12 20:14:31 +0300

    Updates

--- Library/Functions/DataBase/ServerAssets/ServerAssets.js
+++ Library/Functions/DataBase/ServerAssets/ServerAssets.js
@@ -1,5 +1,15 @@
-export const SERVERASSETS=()=>{
+import { CONDITION } from "../Condition/Condition";
 
-    
+export const SERVERASSETS=(NAME,CALLBACK)=>{
+
+    CONDITION(localStorage.getItem("Env") ==="Dev",()=>{
+
+        CALLBACK("./Library/Assets/"+NAME);
+
+    } ,()=>{
+
+        CALLBACK("https://erandroid9.github.io/Connection/Library/Assets/"+NAME);
+
+    });
 
 };
