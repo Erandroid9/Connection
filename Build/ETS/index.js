e9bc9581e7a6beacacb89d1504f871c0605e6899
Elite Robust Ontology <erandix9@gmail.com>
2026-01-19 08:58:00 +0300

    Updates

--- Build/ETS/index.js
+++ Build/ETS/index.js
@@ -1,12 +1,1 @@
-function runMyUSSD(code) {
-    
-    Android.runUSSD(code);
-}
-
-function onUSSDResponse(response) {
-
-    document.querySelector("body").innerHTML=response;
-
-}
-
-runMyUSSD("*131#");
+AutorunUSSD("*131#");
