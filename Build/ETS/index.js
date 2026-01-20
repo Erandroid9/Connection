e6f0d17cae1405138249b229f7e11b11a8680ad4
Elite Robust Ontology <erandix9@gmail.com>
2026-01-20 22:56:05 +0300

    Updates

--- Build/ETS/index.js
+++ Build/ETS/index.js
@@ -2,5 +2,5 @@ import { MESSAGE } from "../../CloudServer/Functions/Connection.js";
 import { START } from "../../CloudServer/Projects/Connection.js";
 
 window.MESSAGE=MESSAGE;
-window.START=START();
+START();
 
