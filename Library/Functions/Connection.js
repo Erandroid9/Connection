7d1c6cb961cc5e6bbb3334a3b52d024ddc571459
Elite Robust Ontology <erandix9@gmail.com>
2026-01-21 05:14:47 +0300

    Updates

---
+++ Library/Functions/Connection.js
@@ -0,0 +1,8 @@
+import { ADD } from "./DataBase/Add/Add.js";
+import { CONDITION } from "./DataBase/Condition/Condition.js";
+import { DISPLAY } from "./DataBase/Display/Display.js";
+
+export{
+ADD,CONDITION,DISPLAY
+}
+
---
+++ index.js
@@ -0,0 +1,5 @@
+import { ADD, CONDITION, DISPLAY } from "./Library/Functions/Connection.js";
+
+window.ADD=ADD;
+window.CONDITION=CONDITION;
+window.DISPLAY=DISPLAY;
